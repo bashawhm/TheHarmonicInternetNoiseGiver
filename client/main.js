3ba96b1e5111dc0dee39c7ac5fe2ca5e3ad81fd3
Reece Emero <reece.emero98@gmail.com>
2018-12-03 19:50:53 -0500

    Establish websocket connection with server and send data as JSON

--- client/main.js
+++ client/main.js
@@ -1,5 +1,22 @@
 var setupLobbyBtn = document.querySelector("#setup-lobby-btn")
 console.log(setupLobbyBtn)
 setupLobbyBtn.addEventListener("click", function(){
-    // sample code
-}); 
+    console.log("Button pressed.")
+})
+
+// Create WebSocket connection.
+const socket = new WebSocket('ws://localhost:80/socket')
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    console.log("Socket connection established...")
+    var message = {
+        command: "HELLO SERVER"
+    }
+    socket.send(JSON.stringify(message))
+})
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+})
